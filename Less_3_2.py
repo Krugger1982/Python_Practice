bc87a6625f35a0fbe47c60ece3c1a03f6cb83f63
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-10-02 18:16:02 +0300

    Create Less_3_2.py

---
+++ Less_3_2.py
@@ -0,0 +1,16 @@
+import random
+summ = 0
+for i in range(2):
+    x = str(random.randint(1, 10)) + '.txt'
+    print(x)
+    with open(x, 'rt') as a:
+        try:
+            s = a.readline()
+            while s!= '':
+                summ += int(s.rstrip())
+                s = a.readline()
+        except TypeError:
+            print ('В файле ', x, ' ошибка данных')
+        finally:
+            a.close()
+print(summ)
